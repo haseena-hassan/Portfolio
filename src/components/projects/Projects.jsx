513504f339477b0dcb942acc4ca3f6f230a64694
haseena-hassan <haseena2199@gmail.com>
2024-05-03 15:24:21 +0530

    updates

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -73,6 +73,67 @@ const Projects = () => {
 
                 <div className="projects__content">
                     <div>
+                        <i className="uil uil-edit projects__icon"></i>
+                        <h3 className="projects__title">FinanceFolio <br/> Expense Tracker</h3>
+                    </div>
+                    <span className="projects__button" onClick={() => setToggle(3)} >View More
+                    <i className="uil uil-arrow-right projects__button-icon"></i>
+                    </span>
+                    
+                    <div className={toggle === 3 ? "projects__modal active-modal" : "projects__modal"}>
+                        <div className="projects__modal-content">
+                            <i className="uil uil-times projects__modal-close" onClick={() => setToggle(0)} ></i>
+                            <h3 className="projects__modal-title">FinanceFolio</h3>
+                            <div className="projects__modal-subtitle">
+                                <p className="projects__modal-description">
+                                    An expense tracking application for personal finance
+                                </p>
+                                <a href="https://github.com/haseena-hassan/FinanceFolio-Backend" target='_blank'><i class='bx bx-link-alt'></i></a>
+                            </div>
+                            
+
+                            <ul className="projects__modal-items grid">
+                                <li className="projects__modal-item">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        User authentication layer using spring security.
+                                    </p>
+                                </li>
+
+                                <li className="projects__modal-item">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        Simple UI to manage accounts, transactions and expense categories for users.
+                                    </p>
+                                </li>
+
+                                <li className="projects__modal-item">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        Users can visualize the expenses and income with the help of dashboard
+                                    </p>
+                                </li>
+
+                                <li className="projects__modal-item">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        More features to add..
+                                    </p>
+                                </li>
+
+                                <li className="projects__modal-item">
+                                    <i className="uil uil-check-circle projects__modal-icon"></i>
+                                    <p className="projects__modal-info">
+                                        TechStack: SpringBoot, Hibernate, JUnit, MySQL, ReactJS.
+                                    </p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="projects__content">
+                    <div>
                         <i class='uil uil-chat-info projects__icon' ></i>
                         <h3 className="projects__title">Notified <br/> App</h3>
                     </div>
@@ -124,67 +185,6 @@ const Projects = () => {
                                     <i className="uil uil-check-circle projects__modal-icon"></i>
                                     <p className="projects__modal-info">
                                         TechStack: ReactJS, NodeJS, ExpressJs, MongoDB, SendGrid.
-                                    </p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projects__content">
-                    <div>
-                        <i className="uil uil-edit projects__icon"></i>
-                        <h3 className="projects__title">FinanceFolio <br/> Expense Tracker</h3>
-                    </div>
-                    <span className="projects__button" onClick={() => setToggle(3)} >View More
-                    <i className="uil uil-arrow-right projects__button-icon"></i>
-                    </span>
-                    
-                    <div className={toggle === 3 ? "projects__modal active-modal" : "projects__modal"}>
-                        <div className="projects__modal-content">
-                            <i className="uil uil-times projects__modal-close" onClick={() => setToggle(0)} ></i>
-                            <h3 className="projects__modal-title">FinanceFolio</h3>
-                            <div className="projects__modal-subtitle">
-                                <p className="projects__modal-description">
-                                    An expense tracking application for managing and visualizing personal finance
-                                </p>
-                                <a href="https://github.com/haseena-hassan/EDI-Spec-Generator" target='_blank'><i class='bx bx-link-alt'></i></a>
-                            </div>
-                            
-
-                            <ul className="projects__modal-items grid">
-                                <li className="projects__modal-item">
-                                    <i className="uil uil-check-circle projects__modal-icon"></i>
-                                    <p className="projects__modal-info">
-                                        User authentication layer using spring security.
-                                    </p>
-                                </li>
-
-                                <li className="projects__modal-item">
-                                    <i className="uil uil-check-circle projects__modal-icon"></i>
-                                    <p className="projects__modal-info">
-                                        Simple UI to manage accounts, transactions and expense categories for users.
-                                    </p>
-                                </li>
-
-                                <li className="projects__modal-item">
-                                    <i className="uil uil-check-circle projects__modal-icon"></i>
-                                    <p className="projects__modal-info">
-                                        Users can visualize the expenses and income with the help of dashboard
-                                    </p>
-                                </li>
-
-                                <li className="projects__modal-item">
-                                    <i className="uil uil-check-circle projects__modal-icon"></i>
-                                    <p className="projects__modal-info">
-                                        More features to add..
-                                    </p>
-                                </li>
-
-                                <li className="projects__modal-item">
-                                    <i className="uil uil-check-circle projects__modal-icon"></i>
-                                    <p className="projects__modal-info">
-                                        TechStack: Java SpringBoot, Hibernate, JUnit, MySQL, ReactJS, Bootstrap.
                                     </p>
                                 </li>
                             </ul>
